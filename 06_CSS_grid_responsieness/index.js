8d798381df345c0553f79c068b55a1e247ed7d51
panvicka <panvicka@gmail.com>
2020-11-09 19:14:44 +0100

    styling done

--- 06_CSS_grid_responsieness/index.js
+++ 06_CSS_grid_responsieness/index.js
@@ -1,60 +1,87 @@
 const API_ENDPOINT = "https://saurav.tech/NewsAPI/everything/cnn.json";
+const TOTAL_ARTICLES_IN_ONE_FETCH = 6;
+
+const loadMoreButton = document.querySelector(".load-more");
 
 const displayNewsItem = (item, divToAttachTo) => {
-  console.log(item.title);
-  
-const time = item.publishedAt.split('T');
-console.log(time);
-time[1] = time[1].replace('Z', '');
-time[1] = time[1].slice(0, -3);
 
-// time[0] = time[0].replace('-', '.');
 
- 
+  // format time and date
+  const time = item.publishedAt.split("T");
+  time[1] = time[1].replace("Z", "");
+  time[1] = time[1].slice(0, -3);
+
+  // create an article body
   newsContainer = document.createElement("div");
-  newsContainer.classList = "news-item"
+  newsContainer.classList = "news-item";
   newsContainer.innerHTML = `
   <img src="${item.urlToImage}" >
   <h1>${item.title}</h1>
   <p class="info">${time[0]}, ${time[1]}<p>
-  <p class="info" by <span class="author">${item.author || ' '}</span></p>
-  <p class="content">${item.description}
-   
+  <p class="info" by <span class="author">${item.author || " "}</span></p>
+  <p class="content">${item.description}   
   </p>
+  <div class="news-footer"><a href="${
+    item.url
+  }">Click to open the article</a></div>    
+  `;
 
-  <div class="news-footer"><a href="${item.url}">Click to open the article</a></div>
- 
-    
-    
-  `
- 
-  newsContainer.addEventListener('click', ()=> {
-      window.open(item.url);
-  })
- 
+  // when article clicked go to the original page
+  newsContainer.addEventListener("click", () => {
+    window.open(item.url);
+  });
+
+  // add article to the content holder
   divToAttachTo.appendChild(newsContainer);
+
+  return newsContainer;
 };
 
-const loadNews = () => {
+const loadNews = (scrollUp) => {
   fetch(API_ENDPOINT)
     .then((response) => response.json())
     .then((data) => {
-        console.log(data);
+
+      // select the content holder 
       const contentDiv = document.querySelector(".content-container");
 
-      for (let i = 0; i< 10; i++) {
-       
-        const rand = Math.floor(Math.random() *(  data.articles.length-1));
-        console.log(rand);
-        displayNewsItem(data.articles[rand], contentDiv);
- 
+      // this holds the first created article in the batch so I know where to scroll to 
+      let firstCreatedNewsContainer;
+      let i = TOTAL_ARTICLES_IN_ONE_FETCH;
+      while (i > 0) {
+
+        // get a random articles from the fetch data and check if they have title and content not null
+        const rand = Math.floor(Math.random() * (data.articles.length - 1));
+
+        if (
+          data.articles[rand].title !== null &&
+          data.articles[rand].content !== null
+        ) {
+          const createdNewsContainer = displayNewsItem(
+            data.articles[rand],
+            contentDiv
+          );
+          // save a reference to the first article 
+          if (i === TOTAL_ARTICLES_IN_ONE_FETCH) {
+            firstCreatedNewsContainer = createdNewsContainer;
+          }
+          i--;
+        }
       }
-
-  
-      
-
-     
+      //if scroll up allowed, scroll to the article 
+      if (scrollUp) {
+        firstCreatedNewsContainer.scrollIntoView({
+          behavior: "smooth",
+        });
+      }
     });
 };
 
-window.addEventListener("load", loadNews);
+//load more button handle 
+loadMoreButton.addEventListener("click", () => {
+  loadNews(1);
+});
+
+window.addEventListener("load", () => {
+  loadNews(0);
+});
