6a80c64ddff59fec342780bdb9a58868721e7d70
panvicka <panvicka@gmail.com>
2020-11-15 18:27:06 +0100

    fixes #2 square flipping problem

--- 07_magic_squares/index.ts
+++ 07_magic_squares/index.ts
@@ -52,10 +52,9 @@ function invertColor(hex) {
     hex = hex[0] + hex[0] + hex[1] + hex[1] + hex[2] + hex[2];
   }
   if (hex.length !== 6) {
-     //fallback color, psst
-     hex = '#0362fc';
-  
-   }
+    //fallback color, psst
+    hex = "#0362fc";
+  }
   // invert color components
   var r = (255 - parseInt(hex.slice(0, 2), 16)).toString(16),
     g = (255 - parseInt(hex.slice(2, 4), 16)).toString(16),
@@ -74,7 +73,7 @@ function getDivInvertedColor(div: HTMLElement) {
 }
 
 function handleTileClick(e: MouseEvent) {
-   const clickedRow = (<HTMLInputElement>e.target).dataset.row;
+  const clickedRow = (<HTMLInputElement>e.target).dataset.row;
   const clickedCol = (<HTMLInputElement>e.target).dataset.column;
 
   const allRowsDivs = document.querySelectorAll<HTMLElement>(
@@ -84,7 +83,6 @@ function handleTileClick(e: MouseEvent) {
     `[data-column="${clickedCol}"]`
   );
 
- 
   allRowsDivs.forEach((div, i) => {
     if (i != parseInt(clickedCol)) {
       const timeDelay = Math.abs(parseInt(clickedCol) - i);
@@ -103,12 +101,17 @@ function handleTileClick(e: MouseEvent) {
     }
   });
 
- }
+  if (clickedCol === clickedRow) {
+    allColsDivs[clickedCol].style.backgroundColor = getDivInvertedColor(
+      allColsDivs[clickedCol]
+    );
+  }
+}
 
 function generateDivs(div: HTMLElement) {
   const randomColor = "#" + Math.floor(Math.random() * 16777215).toString(16);
   const invertedColor = invertColor(`${randomColor}`);
- 
+
   for (let i = 0; i <= ROWS; i++) {
     const newColDiv = document.createElement("div");
     newColDiv.classList.add("column");
