80d49ab0eb74b69e25d5c5edfbc38404efc970fb
panvicka <panvicka@gmail.com>
2020-12-01 21:24:30 +0100

    basic 09 functionality

--- 09_show_us_your_pics/src/index.ts
+++ 09_show_us_your_pics/src/index.ts
@@ -1,1 +1,20 @@
-console.log("it works ");
+const panels = document.querySelectorAll(".panel");
+
+function handlePanelClick(e: Event) {
+  console.log(e.target);
+  if (e.target) {
+    const divTarget = <HTMLDivElement>e.target;
+
+    if (divTarget.classList.contains("open")) {
+      divTarget.classList.remove("open");
+    } else {
+      divTarget.classList.add("open");
+    }
+  }
+}
+
+panels.forEach((panel) => {
+  panel.addEventListener("click", handlePanelClick);
+});
+
+console.log(panels);
